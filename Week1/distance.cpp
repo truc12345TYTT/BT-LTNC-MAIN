3f7593b8bc6ea165d9b68a6802cfc1bb23200fdb
name <asd@example.com>
2023-03-07 14:44:27 +0700

    Create distance.cpp

---
+++ Week1/distance.cpp
@@ -0,0 +1,14 @@
+/// B01B
+///Bai 1
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    printf("%.2f",sqrt(x*x + y*y));
+    return 0;
+}
